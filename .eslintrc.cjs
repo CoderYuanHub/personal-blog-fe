7c2c5614660e1d4e0da463ea0186b53814364363
leslieXin92 <leslieXin92@gmail.com>
2022-12-12 19:25:39 +0800

    chore: add eslint & prettier & editorconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,31 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+	root: true,
+	"extends": [
+		"plugin:vue/vue3-essential",
+		"eslint:recommended",
+		"@vue/eslint-config-typescript",
+		"@vue/eslint-config-prettier"
+	],
+	parserOptions: {
+		ecmaVersion: "latest"
+	},
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"vue/multi-word-component-names": "off",
+		"no-undef": "off",
+		"@typescript-eslint/no-var-requires": "off",
+		"@typescript-eslint/no-non-null-assertion": "off",
+		"prettier/prettier": [
+			"error",
+			{
+				singleQuote: false,
+				semi: true
+			}
+		]
+	}
+};
