d3f67ccb8e663bcbfd24047bae25c4f067f4a0d1
zhanglx <zhanglx@qianlong2.net>
2022-12-23 15:25:19 +0800

    feat: 封装blogList触底加载

---
+++ src/hooks/useLoadBlogList.ts
@@ -0,0 +1,29 @@
+import { onMounted } from "vue";
+import { getBlogList } from "@/http/api/portal/portal";
+import type { Ref } from "vue";
+import type { IGetBlogListParams, BlogItem } from "@/http/api/portal/type";
+
+interface IProps {
+  params: IGetBlogListParams;
+  originData: Ref<Omit<BlogItem, "content">[]>;
+}
+
+export const useLoadBlogList = (props: IProps) => {
+  const { params, originData } = props;
+
+  const loadMore = async () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+    const scrollHeight = document.documentElement.scrollHeight;
+    if (scrollTop + clientHeight >= scrollHeight) {
+      const { data: { blogList } } = await getBlogList(params);
+      originData.value.push(...blogList);
+      params.pageNum++;
+    }
+  };
+
+  onMounted(async () => {
+    await loadMore();
+    window.addEventListener("scroll", loadMore);
+  });
+};
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -4,13 +4,21 @@
     <ArticleCard />
     <ArticleCard />
     <ArticleCard />
+    <div v-for="item in blogList" :key="item.id" class="m-20">
+      {{ item.title }}
+    </div>
   </div>
   <div class="h-6">22222</div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import ArticleCard from "@/components/article-card";
-import { getBlogList } from "@/http/api/portal/portal";
+import { useLoadBlogList } from "@/hooks/useLoadBlogList";
+import type { Ref } from "vue";
+import type { BlogItem } from "@/http/api/portal/type";
+
+const blogList: Ref<Omit<BlogItem, "content">[]> = ref([]);
 
 const params = {
   type: "public" as "public" | "private",
@@ -18,7 +26,12 @@ const params = {
   pageSize: 10
 };
 
-getBlogList(params);
+const props = {
+  params,
+  originData: blogList
+};
+
+useLoadBlogList(props);
 </script>
 
 <style scoped></style>
